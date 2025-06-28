481e0bac187f7930d4ff9263afcee74a8ab43f3e
Nguyen Huu Loc <locn562836@gmail.com>
2025-06-28 17:33:15 +0700

    add NotificationCreateEvent class and update Kafka consumer configuration

---
+++ src/main/java/vn/vinaacademy/kafka/event/NotificationCreateEvent.java
@@ -0,0 +1,34 @@
+package vn.vinaacademy.kafka.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class NotificationCreateEvent {
+    private String title;
+    private String content;
+    private String targetUrl;
+    private NotificationType type;
+    private UUID userId;
+
+    //    private String hash;  sau update len de hash value valid neu k se bi fake noti
+    public enum NotificationType {
+        SYSTEM,
+        PAYMENT_SUCCESS,
+        COURSE_REVIEW,
+        COURSE_APPROVAL,
+        SUPPORT_REPLY,
+        PROMOTION,
+        FINANCIAL_ALERT,
+        STAFF_REQUEST,
+        INSTRUCTOR_REQUEST
+
+    }
+}
