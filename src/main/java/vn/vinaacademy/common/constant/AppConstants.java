fab2f1926edc1fe80949dee6d59cfc8d5441ba8c
Nguyen Huu Loc <locn562836@gmail.com>
2025-10-01 07:31:44 +0700

    add frontend URL constant and implement slug generation with retry logic

--- src/main/java/vn/vinaacademy/common/constant/AppConstants.java
+++ src/main/java/vn/vinaacademy/common/constant/AppConstants.java
@@ -5,4 +5,6 @@ import lombok.experimental.UtilityClass;
 @UtilityClass
 public class AppConstants {
     public static final String TIME_ZONE = "Asia/Ho_Chi_Minh";
+
+    public static final String FRONTEND_URL = "http://localhost:3000";
 }
